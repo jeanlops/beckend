3d1da74a0287ade2e58a463f7b90f0aa4a731487
jeanlops <jeanlops@hotmail.com>
2019-04-15 20:05:34 -0300

    Adiciona variaveis ambientev5

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -4,8 +4,6 @@ const File = require("../models/File");
 class FileController {
     async store(req, res) {
 
-        console.log(req, res);
-
         const box = await Box.findById(req.params.id);
 
         const file = await File.create({
@@ -17,10 +15,9 @@ class FileController {
 
         await box.save();
 
-        req.io.sockets.in(bod_id).emit("file", file);
+        req.io.sockets.in(box._id).emit("file", file);
 
         return res.json(file);
-
     }
 }
 
